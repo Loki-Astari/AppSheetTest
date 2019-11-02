b7f61b1fd4df80378d4f4272eace36772f5b2bc8
Loki <Loki.Astari@gmail.com>
2019-11-02 15:55:01 -0700

    Add comments

--- main.cpp
+++ main.cpp
@@ -13,11 +13,15 @@
 
 using namespace std::string_literals;
 
+// Some global constants.
 const std::string api       = "https://appsheettest1.azurewebsites.net/sample"s;
 const std::string apiList   = api + "/list"s;
 const std::string apiDetail = api + "/detail/"s;
 const std::regex  phoneNumber("^[0-9][0-9][0-9][- ][0-9][0-9][0-9][- ][0-9][0-9][0-9][0-9]$");
 
+// In this app List and User
+// are simply property bags no nead to have access functions.
+// If this was a more complex app then we would consider having other methods.
 struct List
 {
     std::vector<int>                result;
@@ -35,12 +39,19 @@ struct User
 
 };
 
+// Set up comparison functions used on user.
 const auto youngestUser = [](User const& lhs, User const& rhs){return lhs.age < rhs.age;};
 const auto nameTest     = [](User const& lhs, User const& rhs){return lhs.name < rhs.name;};
 
+// Set up List and User to be read from JSON stream.
 ThorsAnvil_MakeTrait(List, result, token);
 ThorsAnvil_MakeTrait(User, id, name, age, number, photo, bio);
 
+
+// A generic Job.
+// Simply reads an object from an istream.
+// If the read worked then processes it.
+// Note: An istream treats a CURL socket like a standard C++ stream.
 template<typename T>
 class Job
 {
@@ -67,16 +78,23 @@ class Job
         virtual void processesData(std::vector<User>& result, T const& data) = 0;
 };
 
+// A job to handle the details from getting a user object.
 class UserJob: public Job<User>
 {
     public:
         using Job<User>::Job;
         virtual void processesData(std::vector<User>& users, User const& user) override
         {
+            // Check if the phone number is OK.
             if (std::regex_search(user.number, phoneNumber)) {
+
+                // Add the user to a heap.
+                // The heap is ordered by youngest person.
                 users.emplace_back(std::move(user));
                 std::push_heap(users.begin(), users.end(), youngestUser);
                 if (users.size() == 6) {
+                    // If we have more than 5 people the pop the oldest one off.
+                    // Thus we maintain a heap of the 5 youngest people.
                     std::pop_heap(users.begin(), users.end(), youngestUser);
                     users.pop_back();
                 }
@@ -84,6 +102,7 @@ class UserJob: public Job<User>
         }
 };
 
+// A job to handle the list object.
 class ListJob: public Job<List>
 {
     public:
@@ -91,9 +110,12 @@ class ListJob: public Job<List>
         virtual void processesData(std::vector<User>& users, List const& data) override
         {
             if (data.token.get()) {
+                // If we have a continuation token
+                // Then add another job ("ListJob") to the async queue.
                 std::async([&users, job = std::make_unique<ListJob>(apiList + "?token=" + *data.token)](){job->run(users);});
             }
             for(auto const& userId: data.result) {
+                // For each user add a job ("UserJob") to the async queue.
                 std::async([&users, job = std::make_unique<UserJob>(apiDetail + std::to_string(userId))](){job->run(users);});
             }
         }
