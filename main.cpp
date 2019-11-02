31f4b0c5637047f08ede25fb317892d9c8b8f330
Loki <Loki.Astari@gmail.com>
2019-11-02 15:28:45 -0700

    Keep a heap of the youngest users.
        Note we only need the top 5 so pop heap members if there are more than 5.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <string>
 #include <memory>
+#include <algorithm>
 
 #include "ThorSerialize/Traits.h"
 #include "ThorSerialize/SerUtil.h"
@@ -21,18 +22,18 @@ struct List
     std::unique_ptr<std::string>    token;
 };
 
-class User
+struct User
 {
-    private:
         int                     id;
         std::string             name;
         int                     age;
         std::string             number;
         std::string             photo;
         std::string             bio;
-        friend class ThorsAnvil::Serialize::Traits<User>;
-    public:
+
 };
+
+static const auto youngestUser = [](User const& lhs, User const& rhs){return lhs.age < rhs.age;};
 
 ThorsAnvil_MakeTrait(List, result, token);
 ThorsAnvil_MakeTrait(User, id, name, age, number, photo, bio);
@@ -48,29 +49,34 @@ class Job
         virtual ~Job()
         {}
 
-        void run()
+        void run(std::vector<User>& result)
         {
             using ThorsAnvil::Serialize::jsonImport;
             T data;
             if (istream >> jsonImport(data)) {
-                processesData(data);
+                processesData(result, data);
             }
             else {
                 // Do some error handling
             }
         }
 
-        virtual void processesData(T const& data) = 0;
+        virtual void processesData(std::vector<User>& result, T const& data) = 0;
 };
 
 class UserJob: public Job<User>
 {
     public:
         using Job<User>::Job;
-        virtual void processesData(User const& user) override
+        virtual void processesData(std::vector<User>& users, User const& user) override
         {
-            using ThorsAnvil::Serialize::jsonExport;
-            std::cout << jsonExport(user);
+
+            users.emplace_back(std::move(user));
+            std::push_heap(users.begin(), users.end(), youngestUser);
+            if (users.size() == 6) {
+                std::pop_heap(users.begin(), users.end(), youngestUser);
+                users.pop_back();
+            }
         }
 };
 
@@ -78,15 +84,13 @@ class ListJob: public Job<List>
 {
     public:
         using Job<List>::Job;
-        virtual void processesData(List const& data) override
+        virtual void processesData(std::vector<User>& users, List const& data) override
         {
-            using ThorsAnvil::Serialize::jsonExport;
-            std::cout << jsonExport(data);
             if (data.token.get()) {
-                std::async([job = std::make_unique<ListJob>(apiList + "?token=" + *data.token)](){job->run();});
+                std::async([&users, job = std::make_unique<ListJob>(apiList + "?token=" + *data.token)](){job->run(users);});
             }
             for(auto const& userId: data.result) {
-                std::async([job = std::make_unique<UserJob>(apiDetail + std::to_string(userId))](){job->run();});
+                std::async([&users, job = std::make_unique<UserJob>(apiDetail + std::to_string(userId))](){job->run(users);});
             }
         }
 };
@@ -94,7 +98,14 @@ class ListJob: public Job<List>
 int main()
 {
     using ThorsAnvil::Stream::IThorStream;
+    std::vector<User>   users;
 
-    std::async([job = std::make_unique<ListJob>(apiList)](){job->run();});
-
+    std::async([&users, job = std::make_unique<ListJob>(apiList)](){job->run(users);});
+    // This will not return until all async jobs have completed.
+    using ThorsAnvil::Serialize::jsonExport;
+    while(!users.empty()) {
+        std::pop_heap(users.begin(), users.end(), youngestUser);
+        std::cout << jsonExport(users.back()) << "\n";
+        users.pop_back();
+    }
 }
